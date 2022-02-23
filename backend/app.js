ab436ad81ff8ee837e2fe154a41310c51db60ade
Steven Fuchey <Steven30620@hotmail.fr>
2022-02-23 16:19:37 +0100

    premier commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
+
+mongoose
+	.connect(
+		"mongodb+srv://steven:testtest@clustertest.4yuc7.mongodb.net/clusterTest",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
+
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
